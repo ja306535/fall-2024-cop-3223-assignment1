d0ef4e6ce7f98119813ff5687a1667236071ce0c
Jalil <151500208+ja306535@users.noreply.github.com>
2024-09-08 22:54:49 +0000

    ?

--- fracturing.c
+++ fracturing.c
@@ -2,12 +2,13 @@
 #include <math.h>
 #define PI 3.14159
 
+static double x1,w1,x2,w2
 void getValues() {
     printf("Enter x1 and y1 for Point #1: ");
-    scanf("%lf %lf", &x1, &y1);
+    scanf("%lf %lf", &x1, &w1);
 
     printf("Enter x2 and y2 for Point #2: ");
-    scanf("%lf %lf", &x2, &y2);
+    scanf("%lf %lf", &x2, &w2);
 }
 
 int main(int argc,char**argv){
